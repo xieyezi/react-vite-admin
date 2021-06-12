6116a3d9b37c3c7d1abb132be9267f42e6332712
xieyezi <1435398529@qq.com>
2021-06-12 14:23:26 +0800

    feat: make user state persist

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,4 +1,5 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware'
 
 export interface UserState {
 	logged: boolean
@@ -10,35 +11,42 @@ export interface UserState {
 	logout: () => void
 }
 
-const useStore = create<UserState>((set, _) => ({
-	username: '',
-	password: '',
-	role: '',
-	token: '',
-	logged: false,
-	login: ({ username, password, role, token }) => {
-		localStorage.setItem('username', username)
-		localStorage.setItem('password', password)
-		localStorage.setItem('role', role)
-		localStorage.setItem('token', token)
-		set({
-			username,
-			password,
-			role,
-			token,
-			logged: true
-		})
-	},
-	logout: async () => {
-		localStorage.clear()
-		set({
+const useStore = create<UserState>(
+	persist<UserState>(
+		(set, get) => ({
 			username: '',
 			password: '',
 			role: '',
 			token: '',
-			logged: false
-		})
-	}
-}))
+			logged: false,
+			login: ({ username, password, role, token }) => {
+				localStorage.setItem('username', username)
+				localStorage.setItem('password', password)
+				localStorage.setItem('role', role)
+				localStorage.setItem('token', token)
+				set({
+					username,
+					password,
+					role,
+					token,
+					logged: true
+				})
+			},
+			logout: async () => {
+				localStorage.clear()
+				set({
+					username: '',
+					password: '',
+					role: '',
+					token: '',
+					logged: false
+				})
+			}
+		}),
+		{
+			name: 'user'
+		}
+	)
+)
 
 export default useStore
