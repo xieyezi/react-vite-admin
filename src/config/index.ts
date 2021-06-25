5fbabfc1d2909a6ce7a23adc809bfb21b2fc3122
xieyezi <1435398529@qq.com>
2021-06-25 14:34:47 +0800

    feat: chang config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
-const isDev = !isProd
-// 开发环境
-const SERVER_HOST_DEV = ''
-
-// 生产环境
-const SERVER_HOST_PROD = ''
-
-const SERVER_API_URL = isProd ? SERVER_HOST_PROD : SERVER_HOST_DEV
-
-const authKey = 'Authorization'
-
 const config = {
-	isDev,
-	isProd,
-	SERVER_API_URL,
-	authKey
+	apiPrefix: '/api',
+	authKey: 'Authorization'
 }
 export default config
